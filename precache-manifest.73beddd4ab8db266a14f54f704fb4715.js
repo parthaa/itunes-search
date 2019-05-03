b55975518d95646ae77cb9b100416f767ec07aee
Partha Aji <paji@redhat.com>
2019-05-03 01:44:59 -0400

    Updates

---
+++ precache-manifest.73beddd4ab8db266a14f54f704fb4715.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a691bc40f3a65f113063f27d8c63a5",
+    "url": "/itunes-search/index.html"
+  },
+  {
+    "revision": "66f4e2a2c2cd9c282cf8",
+    "url": "/itunes-search/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "c8b6df0a6236f2d60a0d",
+    "url": "/itunes-search/static/js/2.eb63f94b.chunk.js"
+  },
+  {
+    "revision": "66f4e2a2c2cd9c282cf8",
+    "url": "/itunes-search/static/js/main.c322ebd5.chunk.js"
+  },
+  {
+    "revision": "de994bfcdc8b145ce860",
+    "url": "/itunes-search/static/js/runtime~main.a2918ef2.js"
+  }
+]);
